50d6bb48f8aecaf65d2d1e2c24b1b665fee0d41f
emran <shuvoorba28@gmail.com>
2023-10-04 16:24:12 +0600

    initial set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyC3wfLms3wBEJkVjG6DQvsXeIBTT0HO2PU",
+    authDomain: "react-dragon-news-31580.firebaseapp.com",
+    projectId: "react-dragon-news-31580",
+    storageBucket: "react-dragon-news-31580.appspot.com",
+    messagingSenderId: "724457043270",
+    appId: "1:724457043270:web:08a676eb513ccddbaca147"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app; 
