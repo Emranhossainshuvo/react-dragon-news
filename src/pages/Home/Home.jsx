8516d9c619ae20e3d904a8056ff6006ff0fbfe71
emran <shuvoorba28@gmail.com>
2023-10-04 19:28:03 +0600

    folder structure created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h3 className="text-3xl font-poppins font-semibold">This is home</h3>
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="col-span-2">
+                    <h2 className="text-4xl">News coming soon...</h2>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">Left side</h2>            
+        </div>
+    );
+};
+
+export default LeftSideNav;
